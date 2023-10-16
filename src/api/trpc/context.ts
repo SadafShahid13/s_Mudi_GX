c3b16cc37b8dbb1d5f299a7d5d98a5f15f8ac4eb
Sadafshahid13 <borshon.sadafshahid@gmail.com>
2023-10-16 12:45:13 +0600

    Changed Context for auth

--- src/api/trpc/context.ts
+++ src/api/trpc/context.ts
@@ -1,11 +1,17 @@
 import { inferAsyncReturnType } from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { pool } from "../../../db";
+import { returnOf } from "scope-utilities";
 
+export enum UserRole {
+  Admin = "Admin",
+  User = "User",
+}
 export type Context = {
   db: typeof pool;
   auth: {
     isAuthenticated: boolean;
+    role?: UserRole;
   };
 };
 
@@ -13,9 +19,45 @@ export async function createContext({
   req,
   res,
 }: trpcExpress.CreateExpressContextOptions): Promise<Context> {
-  const auth = {
-    isAuthenticated: true,
-  };
+  const auth:
+    | {
+        isAuthenticated: false;
+      }
+    | {
+        isAuthenticated: true;
+        role: UserRole;
+      } = await returnOf(async () => {
+    if ("User") {
+      try {
+        return {
+          isAuthenticated: true,
+          role: UserRole.User,
+        };
+      } catch (error) {
+        return {
+          isAuthenticated: false,
+        };
+      }
+    } else if ("Admin") {
+      try {
+        return {
+          isAuthenticated: true,
+          role: UserRole.Admin,
+        };
+      } catch (error) {
+        return {
+          isAuthenticated: false,
+        };
+      }
+    } else {
+      return { isAuthenticated: false };
+    }
+  });
+
+  //     {
+  //   isAuthenticated: true, //need changes here
+  //   role: UserRole.Admin,
+  // };
 
   return { db: pool, auth };
 }
--- src/api/trpc/router.ts
+++ src/api/trpc/router.ts
@@ -1,5 +1,15 @@
-import { router } from "./index.js";
-
-export const appRouter = router({});
+import { router } from "./index";
+import { clientsRouter } from "./procedures/clients/index";
+import { contractsRouter } from "./procedures/contracts/index";
+// import { usersRouter } from "./procedures/users/index";
+// import { shopsRouter } from "./procedures/shops/index";
+// import { spacesRouter } from "./procedures/spaces/index";
+export const appRouter = router({
+  clients: clientsRouter,
+  contracts: contractsRouter,
+  //   users : usersRouter,
+  //   shops : shopsRouter,
+  //   spaces : spacesRouter,
+});
 
 export type AppRouter = typeof appRouter;
